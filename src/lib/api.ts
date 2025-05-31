338988cb75db3a21d017ee98bb5c7cefd79eb549
duswp <cyj229988@gmail.com>
2025-05-31 18:15:05 +0900

    🔥 Feat: 사용하지 않는 api 파일 삭제

--- src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: 'http://14.63.178.146:8080', // 백엔드 주소
-    headers: { 'Content-Type': 'application/json' },
-});
-
-export default api;
